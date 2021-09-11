07d8f4937ef1573811287fcab19ac1dbf22fbfff
jorge <jorgecuenca.unity@gmail.com>
2021-09-11 08:00:09 -0500

    jk

---
+++ lib/pages/home.page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Home")),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Container(
+                child: Text("Zona de resultados"),
+                color: Colors.blue,
+              ),
+            ),
+            Expanded(
+                flex: 1,
+                child: Container(
+                  child: Text("Zona de ingreso"),
+                  color: Colors.red,
+                )
+            ),
+            Expanded(
+                flex: 2,
+                child:           Container(
+                  child: Text("Zona de botones"),
+                  color: Colors.green,
+                  height: 200,
+                )
+            )
+          ],
+        )
+    );
+  }
+}
